e2bed5167f07cb0e326d5249275df46155571ce2
Bayma Bruno <bayma@Baymas-MacBook-Air.local>
2020-02-13 21:32:04 -0300

    Projeco criado com autenticação

---
+++ src/application.ts
@@ -0,0 +1,102 @@
+import { BootMixin } from '@loopback/boot';
+import { ApplicationConfig, BindingKey } from '@loopback/core';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
+import { RepositoryMixin } from '@loopback/repository';
+import { RestApplication } from '@loopback/rest';
+import { ServiceMixin } from '@loopback/service-proxy';
+import path from 'path';
+import { AuthenticationSequence } from './sequence';
+import {
+  AuthenticationComponent, registerAuthenticationStrategy,
+} from '@loopback/authentication';
+import { JWTAuthenticationStrategy } from './authentication-strategies/jwt-strategy';
+import { TokenServiceBindings, TokenServiceConstants, PasswordHasherBindings, UserServiceBindings } from './keys';
+import { JWTService } from './services/jwt-service';
+import { BcryptHasher } from './services/hash.password.bcryptjs';
+import { MyUserService } from './services/user-service';
+import { SECURITY_SCHEME_SPEC } from './utils/security-spec';
+
+/**
+ * Information from package.json
+ */
+export interface PackageInfo {
+  name: string;
+  version: string;
+  description: string;
+}
+export const PackageKey = BindingKey.create<PackageInfo>('application.package');
+
+const pkg: PackageInfo = require('../package.json');
+
+export class BaseProjectLb4Application extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    super(options);
+
+    this.api({
+      openapi: '3.0.0',
+      info: { title: pkg.name, version: pkg.version },
+      paths: {},
+      components: { securitySchemes: SECURITY_SCHEME_SPEC },
+      servers: [{ url: '/' }],
+    });
+
+    // Bind authentication component related elements
+    this.component(AuthenticationComponent);
+
+    // Set up the custom sequence
+    this.sequence(AuthenticationSequence);
+
+    // Resiter JWTAuthentication Strategy
+    registerAuthenticationStrategy(this, JWTAuthenticationStrategy);
+
+    this.setUpBindings();
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.bind(RestExplorerBindings.CONFIG).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+    };
+  }
+
+  setUpBindings(): void {
+
+    // Bind package.json to the application context
+    this.bind(PackageKey).to(pkg);
+
+    this.bind(TokenServiceBindings.TOKEN_SECRET).to(
+      TokenServiceConstants.TOKEN_SECRET_VALUE,
+    );
+
+    this.bind(TokenServiceBindings.TOKEN_EXPIRES_IN).to(
+      TokenServiceConstants.TOKEN_EXPIRES_IN_VALUE,
+    );
+
+    this.bind(TokenServiceBindings.TOKEN_SERVICE).toClass(JWTService);
+
+    // Bind bcrypt hash services - utilized by 'UserController' and 'MyUserService'
+    this.bind(PasswordHasherBindings.ROUNDS).to(10);
+    this.bind(PasswordHasherBindings.PASSWORD_HASHER).toClass(BcryptHasher);
+
+    this.bind(UserServiceBindings.USER_SERVICE).toClass(MyUserService);
+  }
+}
+
