944b100b1e8d954cfe7c8b0f2069c4dae09ac820
Bayma Bruno <bayma@Baymas-MacBook-Air.local>
2020-03-10 21:56:20 -0300

    Implementado acesso para openapi pelo .env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {BaseProjectLb4Application} from './application';
-import {ApplicationConfig} from '@loopback/core';
+import { BaseProjectLb4Application } from './application';
+import { ApplicationConfig } from '@loopback/core';
 
-export {BaseProjectLb4Application};
+export { BaseProjectLb4Application };
 
 export async function main(options: ApplicationConfig = {}) {
   const app = new BaseProjectLb4Application(options);
@@ -9,6 +9,17 @@ export async function main(options: ApplicationConfig = {}) {
   await app.start();
 
   const url = app.restServer.url;
+  const config = app.restServer.config;
+
+  if (config.openApiSpec.disabled === true) {
+    console.log(`> OpenApi disabled \n`);
+  } else {
+    console.log('> OpenApi is running at:')
+    console.log(`- ${url}/explorer`);
+    console.log(`- ${url}/openapi.json`);
+    console.log(`- ${url}/openapi.yaml \n`);
+  }
+
   console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
 
