c7dfd5617a006dafc497a6a2c700c8cd8ff1c605
Bayma Bruno <bayma@Baymas-MacBook-Air.local>
2020-03-15 12:40:20 -0300

    Criado ping.controller.specs

---
+++ src/controllers/specs/ping.controller.specs.ts
@@ -0,0 +1,57 @@
+import { ResponseObject } from '@loopback/rest';
+
+export const PingResponse: ResponseObject = {
+  description: 'Ping Response',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        title: 'PingResponse',
+        properties: {
+          greeting: { type: 'string' },
+          date: { type: 'string' },
+          url: { type: 'string' },
+          headers: {
+            type: 'object',
+            properties: {
+              'Content-Type': { type: 'string' },
+            },
+            additionalProperties: true,
+          },
+        },
+      },
+    },
+  },
+};
+
+export const MailResponse: ResponseObject = {
+  description: 'Mail Response',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        title: 'MailResponse',
+        properties: {
+          messageId: { type: 'string' }
+        }
+      }
+    }
+  }
+};
+
+export const MailErrorResponse: ResponseObject = {
+  description: 'Mail Response Error',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        title: 'MailErrorResponse',
+        properties: {
+          'statusCode': { type: 'number' },
+          'name': { type: 'string' },
+          'message': { type: 'object' },
+        }
+      }
+    }
+  }
+}
